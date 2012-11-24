c058f313c859c10633922ad9ca8cd16a861c497f
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2012-11-23 22:43:23 -0500

    Update gk-query to use GObject Introspection; start scripts for Synergy

--- gk-query.py
+++ gk-query.py
@@ -8,7 +8,11 @@ import curses
 import re
 import sys
 
-import gnomekeyring as gk
+from gi.repository import GnomeKeyring as gk
+
+
+# keyring to use; if 'None', GnomeKeyring selects the default
+KEYRING = None
 
 
 def query_results(screen, search_re):
@@ -26,21 +30,29 @@ def query_results(screen, search_re):
     # search the login keyring; can't use gk.find_items_sync here because it has
     #   no regex functionality
     found = False
-    for item_id in gk.list_item_ids_sync('login'):
-        item = gk.item_get_info_sync('login', item_id)
+    for item_id in gk.list_item_ids_sync(KEYRING)[1]:
+        rc, item = gk.item_get_info_sync(KEYRING, item_id)
         # match the item's name
         if search_re.match(item.get_display_name()):
             found = True
             # prompt the user
-            screen.addstr("Display secret for '%s'? [y/N/q] " % \
-              item.get_display_name())
+            screen.addstr("Display secret for '{}'? [y/N/q] ".format(
+                                               item.get_display_name()))
             # single character input
             key = screen.getkey()
             if key in ['y', 'Y']:
                 # display the secret
-                screen.addstr('\n' + item.get_secret() + '\n')
+                #commented: the following seems to be broken:
+                ## also display the username, if available
+                #attrs = gk.Attribute.list_new()
+                #gk.item_get_attributes_sync(KEYRING, item_id, attrs)
+                #try:
+                #    screen.addstr('{}'.format(attrs['username_value']))
+                #except KeyError:
+                #    pass
+                screen.addstr('\n{}\n'.format(item.get_secret()))
             elif key in ['q', 'Q']:
-                # quitting; push a key to the next getch for a speedy exit
+                # quit: push a key to the next getch for a speedy exit
                 curses.ungetch(10)
                 break
             else:
@@ -56,21 +68,22 @@ def query_results(screen, search_re):
 if __name__ == '__main__':
     # check that gnome-keyring is available
     if not gk.is_available():
-        print '%s: gnome-keyring is unavailable' % sys.argv[0]
+        print('{}: gnome-keyring is unavailable'.format(sys.argv[0]))
         sys.exit(1)
     # unlock the login keyring, if necessary
     was_locked = False
-    if gk.get_info_sync('login').get_is_locked():
+    if gk.get_info_sync(KEYRING)[1].get_is_locked():
         was_locked = True
         import getpass
         gk.unlock_sync('login', getpass.getpass(prompt=
           'Enter password for login keyring: '))
     # use curses.wrapper to ensure things are taken care of in case of a
     #   KeyboardInterrupt or other exception
-    found = curses.wrapper(query_results, re.compile(r'.*'+ sys.argv[1] +'.*'))
+    found = curses.wrapper(query_results, re.compile(r'.*{}.*'.format(
+                                                          sys.argv[1])))
     # helpful feedback
     if not found:
-        print "No matches for search term '%s'." % sys.argv[1]
+        print("No matches for search term '{}'.".format(sys.argv[1]))
     # relock the keyring if necessary
     if was_locked:
-        gk.lock_sync('login')
+        gk.lock_sync(KEYRING)
